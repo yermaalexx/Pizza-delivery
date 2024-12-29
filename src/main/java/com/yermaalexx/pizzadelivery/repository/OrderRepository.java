dafba8f50517781f359b1ad72d93683898421fde
Yermakov Olexii <yermakov80@gmail.com>
2024-12-29 17:25:48 +0200

    Initial commit

--- src/main/java/com/yermaalexx/pizzadelivery/repository/OrderRepository.java
+++ src/main/java/com/yermaalexx/pizzadelivery/repository/OrderRepository.java
@@ -1,8 +1,6 @@
 package com.yermaalexx.pizzadelivery.repository;
 
 import com.yermaalexx.pizzadelivery.model.PizzaOrder;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
