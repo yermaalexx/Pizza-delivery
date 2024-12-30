e458c6e2a8af466ae2bcf0b9b7d913c9807d3263
Yermakov Olexii <yermakov80@gmail.com>
2024-12-30 23:02:29 +0200

    tests 1

--- src/test/java/com/yermaalexx/pizzadelivery/HomeControllerTest.java
+++ src/test/java/com/yermaalexx/pizzadelivery/HomeControllerTest.java
@@ -1,14 +1,15 @@
 package com.yermaalexx.pizzadelivery;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import com.yermaalexx.pizzadelivery.controller.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
@@ -17,10 +18,14 @@ public class HomeControllerTest {
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"))
-                .andExpect(content().string(containsString("Welcome to...")));
+                .andExpect(content().string(containsString("Welcome to ")))
+                .andExpect(content().string(containsString("src=\"/images/Pizza.png\"")))
+                .andExpect(content().string(containsString("href=\"/login\"")))
+        ;
     }
 }
